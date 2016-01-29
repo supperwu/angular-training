4ffc45c4dbef91fc68640cb5812e7de685959ad6
andy.lv <andy.lv@live.com>
2016-01-29 14:48:39 +0800

    base training for directive.

---
+++ Directive/direct-scope.js
@@ -0,0 +1,15 @@
+angular.module('myApp', [])
+  .directive('myDirective', function() {
+    return {
+      restrict: 'A',
+      template: 'Inside myDirective, isolate scope: {{ myProperty }}',
+      scope: {}
+    };
+  })
+  .directive('myInheritScopeDirective', function() {
+    return {
+      restrict: 'A',
+      template: 'Inside myDirective, isolate scope: {{ myProperty }}',
+      scope: true
+    };
+  });

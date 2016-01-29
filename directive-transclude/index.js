faff295ea6954a6583c2aaf354eca83fe995fff7
andy.lv <andy.lv@live.com>
2016-01-29 15:11:54 +0800

    directive transclude

---
+++ directive-transclude/index.js
@@ -0,0 +1,17 @@
+angular.module('myApp', [])
+  .directive('sidebox', function() {
+    return {
+      restrict: 'EA',
+      scope: {
+        title: '@'
+      },
+      transclude: true,
+      template: '<div class="sidebox">\
+                    <div class="content">\
+                    <h2 class="header">{{ title }}</h2>\
+                    <span class="content" ng-transclude>\
+                    </span>\
+                    </div>\
+                  </div>'
+    };
+  });

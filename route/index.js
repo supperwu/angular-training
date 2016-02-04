36efa483e773b8031e2c3b0a52b9d18d7c1da119
microlv <andy.lv@live.com>
2016-02-04 12:20:38 +0800

    angular-route

---
+++ route/index.js
@@ -0,0 +1,24 @@
+angular.module('myApp', []).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/home.html',
+      controller: 'HomeController'
+    })
+    .when('/login', {
+      templateUrl: 'views/login.html',
+      controller: 'LoginController'
+    })
+    .when('/dashboard', {
+      templateUrl: 'views/dashboard.html',
+      controller: 'DashboardController',
+      resolve: {
+        user: function(SessionService) {
+          return SessionService.getCurrentUser();
+        }
+      }
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+}]);

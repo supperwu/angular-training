5568fcbbb62c95088abbf2041c7dcfb0aca80a4f
microlv <andy.lv@live.com>
2016-02-15 15:26:17 +0800

    add route

--- route/index.js
+++ route/index.js
@@ -1,24 +1,16 @@
 angular.module('myApp', []).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider
-    .when('/', {
-      templateUrl: 'views/home.html',
-      controller: 'HomeController'
-    })
-    .when('/login', {
-      templateUrl: 'views/login.html',
-      controller: 'LoginController'
-    })
-    .when('/dashboard', {
-      templateUrl: 'views/dashboard.html',
-      controller: 'DashboardController',
-      resolve: {
-        user: function(SessionService) {
-          return SessionService.getCurrentUser();
-        }
-      }
-    })
-    .otherwise({
-      redirectTo: '/'
-    });
-}]);
+  config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/home.html'
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html'
+      })
+      .when('/dashboard', {
+        templateUrl: 'views/dashboard.html'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
